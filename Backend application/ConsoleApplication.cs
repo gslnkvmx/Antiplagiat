b9dbca3da820c35a97673e4b69bcb07313f0cd6e
DenisStepanidenko <110686828+DenisStepanidenko@users.noreply.github.com>
2024-01-04 23:57:05 +0300

    Update ConsoleApplication.cs

--- Backend application/ConsoleApplication.cs
+++ Backend application/ConsoleApplication.cs
@@ -2,9 +2,10 @@
 {
     internal class Program
     {
-          // здесь будет описана последовательность действий антиплагиата
+        // здесь будет описана последовательность действий антиплагиата
         public static void Main(string[] args)
         {
+           
             // инициализация объектов Input и Output
             var input = new Input();
             
@@ -19,9 +20,25 @@
                 switch (choice)
                 {
                      case 1:
-                         // далее спрашиваем у пользователя каким способом он хочет ввести число
-                         input.GetStringOrFile(out choice);
+                         // далее спрашиваем у пользователя каким способом он хочет ввести текст в антиплагиат
+                         string text;
+                         while (true)
+                         {
+                             try
+                             {
+                                 input.GetStringOrFile(out choice);
+                                 text = input.GetCurrentText(choice);
+                                 break;
+                             }
+                             catch (IOException)
+                             {
+                                 Console.WriteLine("Произошла непредвиденная ошибка при загрузке текста, вероятно ошибка произошла при открытии файла. Повторите ,пожалуйста, ввод");
+                             }
+                         }
+                         // на данном этапе у нас есть текст
+                         //Console.WriteLine(text);
                          
+                         // далее нужно обрабатывать данный текст
                          break;
                      case 2:
                          // in future
