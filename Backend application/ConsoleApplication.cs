3a6e68a29579a33ae5143c8899f4e045229a0590
DenisStepanidenko <110686828+DenisStepanidenko@users.noreply.github.com>
2024-01-04 23:20:32 +0300

    Update ConsoleApplication.cs

--- Backend application/ConsoleApplication.cs
+++ Backend application/ConsoleApplication.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
+          // здесь будет описана последовательность действий антиплагиата
         public static void Main(string[] args)
         {
+            // инициализация объектов Input и Output
+            var input = new Input();
             
+            
+            
+            while (true)
+            {
+                int choice;
+                input.StartChoice(out choice);
+                
+                // на данном шаге мы уверены что в choice лежит, либо 1, либо 2, либо 3
+                switch (choice)
+                {
+                     case 1:
+                         // далее спрашиваем у пользователя каким способом он хочет ввести число
+                         input.GetStringOrFile(out choice);
+                         
+                         break;
+                     case 2:
+                         // in future
+                         break;
+                     case 3:
+                         Console.WriteLine("Спасибо за работу в нашем антиплагиате!");
+                         return;
+                }
+            }
         }
     }
 }
